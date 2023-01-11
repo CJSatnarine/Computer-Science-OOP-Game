e22dac2e208e2098bf2739202eabc84e36288e06
CJ Satnarine <satnarinecj101@gmail.com>
2023-01-10 23:43:48 -0500

    Changed array to Arraylist (got sent to bed again)

--- src/App/Slug.java
+++ src/App/Slug.java
@@ -3,6 +3,8 @@ package App;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
+
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Slug {
@@ -23,23 +25,30 @@ public class Slug {
     }
 
     public void draw(Graphics2D g2, int num) {
-        Rectangle[] rects = new Rectangle[num];
+        //Rectangle[] rects = new Rectangle[num];
+        ArrayList <Rectangle> rect = new ArrayList<Rectangle>();
 
         //For loop to create/draw num amount of enemies
-        for (int i = 0; i < num; i++) {
-            rects[i] = new Rectangle(x, y, width, height);
+        for (int i = 0; i <= num; i++) {
+            //rects[i] = new Rectangle(x, y, width, height);
+            rect.add(new Rectangle(x, y, width, height));
 
             g2.setColor(Color.WHITE);
+            g2.fill(rect.get(i));
             //g2.fillRect(x, y + 100, width, height);
-            g2.fill(rects[i]);
+            //g2.fill(rects[i]);
         }
 
-        y = rand.nextInt(0, 600);
+        //y = rand.nextInt(0, 600);
 
         // g2.setColor(Color.white);
         // g2.fillRect(x, y + height + 200, width, height);
 
         //g2.dispose(); //causes the JFrame window to be destroyed and cleaned up by the operating system.
+    }
+
+    public void createSprite(Graphics2D g2, int spriteX, int spriteY, int spriteWidth, int spriteHeight) {
+        Rectangle r = new Rectangle(spriteX, spriteY, spriteWidth, spriteHeight);
     }
 
     public void move() {
