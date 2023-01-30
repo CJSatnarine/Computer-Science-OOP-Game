687ae28d193f51f93416a97d332898bb80b11256
CJ Satnarine <satnarinecj101@gmail.com>
2023-01-29 19:28:10 -0500

    Commented out code to get player position in AE

--- src/App/AdvancedEnemy.java
+++ src/App/AdvancedEnemy.java
@@ -9,11 +9,11 @@ import java.util.Random;
 public class AdvancedEnemy extends Enemy {
     private Random random = new Random();
     private double angle;
-    private Player p; //IThis is null.  
+    // private Player p; //IThis is null.  
 
-    //I need to somehow get playerX and playerY positions. 
-    private int playerXPos = p.getPlayerX();
-    private int playerYPos = p.getPlayerY();
+    // //I need to somehow get playerX and playerY positions. 
+    // private int playerXPos = p.getPlayerX();
+    // private int playerYPos = p.getPlayerY();
 
 
     public AdvancedEnemy(int x, int y, int width, int height, int speed) {
@@ -26,10 +26,10 @@ public class AdvancedEnemy extends Enemy {
         g2.fillRect(x - 50, y + 50, width, height);
     }
 
-    public void move() {
-        angle = Math.atan2(y - playerYPos, x - playerXPos);
+    // public void move() {
+    //     angle = Math.atan2(y - playerYPos, x - playerXPos);
         
-        y -= speed * Math.sin(angle);
-        x -= speed * Math.cos(angle);
-    }
+    //     y -= speed * Math.sin(angle);
+    //     x -= speed * Math.cos(angle);
+    // }
 }
