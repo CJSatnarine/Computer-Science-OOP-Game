94c51ccdbda8834da1711a591894d5ee84116cfb
CJSatnarine <satnarinecj101@gmail.com>
2023-08-05 17:42:17 -0400

    Changed the scope of some variables in Healthbar.

--- src/App/Healthbar.java
+++ src/App/Healthbar.java
@@ -6,17 +6,17 @@ import java.awt.Graphics2D;
 public class Healthbar {
     int health;
 
-    int x = 10;
-    int y = 10;
-    int width = health * 10;
-    int height = 20;
-
     public Healthbar(int playerHealth) {
         health = playerHealth;
     }
     
     public void draw(Graphics2D g2) {
+        int x = 10;
+        int y = 10;
+        int width = health * 10;
+        int height = 20;
+
         g2.setColor(Color.RED);
-        g2.fillRect(10, 10, health * 10, 20);
+        g2.fillRect(x, y, width, height);
     }
 }
